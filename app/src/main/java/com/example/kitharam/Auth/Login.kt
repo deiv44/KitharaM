48307298f78589c1e1ff2e81674dac71ba69454f
deiv44 <117960521+deiv44@users.noreply.github.com>
2024-03-04 12:44:32 +0800

    commiti Profite Xml user and tutor

--- app/src/main/java/com/example/kitharam/Auth/Login.kt
+++ app/src/main/java/com/example/kitharam/Auth/Login.kt
@@ -1,32 +1,75 @@
 package com.example.kitharam.Auth
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.kitharam.R
+import android.app.AlertDialog
+import android.widget.EditText
+import android.widget.Toast
 import com.example.kitharam.UserActivity
 
 class Login : AppCompatActivity() {
+
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+    private lateinit var btnforgot: Button
+    private var email: String = ""
+    private var password: String = ""
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        etEmail = findViewById(R.id.etemail)
+        etPassword = findViewById(R.id.etpassword)
+        btnLogin = findViewById(R.id.Login)
+        btnRegister = findViewById(R.id.signup)
+        btnforgot = findViewById(R.id.forgot)
 
-        val loginbttn = findViewById<Button>(R.id.Login)
-        val signupbttn = findViewById<Button>(R.id.signup)
-
-
-        loginbttn.setOnClickListener {
-            val intent = Intent(this, UserActivity::class.java)
-            startActivity(intent)
+        btnLogin.setOnClickListener {
+            checkLogin()
         }
-
-        signupbttn.setOnClickListener {
+        btnRegister.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+        btnforgot.setOnClickListener {
+            val intent = Intent(this, ForgotpassFragment::class.java)
+            startActivity(intent)
+        }
+    }
 
+    private fun checkLogin() {
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+        if (email.isEmpty() || password.isEmpty()) {
+            alertFail("Email and Password is required.")
+        } else {
+            sendLogin()
+        }
+    }
 
+    private fun sendLogin() {
+        Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, UserActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun alertFail(s: String) {
+        AlertDialog.Builder(this)
+            .setTitle("Failed")
+            .setIcon(R.drawable.baseline_info_24)
+            .setMessage(s)
+            .setPositiveButton("OK") { dialog, which ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
