9abac1c67a02ec844d1720535686fe039d521148
deiv44 <117960521+deiv44@users.noreply.github.com>
2024-03-14 10:20:11 +0800

    commit login tr

--- app/src/main/java/com/example/kitharam/Auth/LoginTr.kt
+++ app/src/main/java/com/example/kitharam/Auth/LoginTr.kt
@@ -1,32 +1,95 @@
 package com.example.kitharam.Auth
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.kitharam.R
 import com.example.kitharam.TutorActivity
+import com.example.kitharam.UserActivity
+import com.example.kitharam.api.LoginResponse
+import com.example.kitharam.api.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class LoginTr : AppCompatActivity() {
+
+
+    private lateinit var etemail: EditText
+    private lateinit var etpassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var btnsignup: Button
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_tr)
 
 
-        val loginbttn = findViewById<Button>(R.id.Login)
-        val signupbttn = findViewById<Button>(R.id.signup)
+        etemail = findViewById(R.id.etemail)
+        etpassword = findViewById(R.id.etpassword)
+        btnlogin = findViewById(R.id.logintr)
+        btnsignup = findViewById(R.id.signuptr)
 
-        loginbttn.setOnClickListener {
-            val intent = Intent(this,  TutorActivity::class.java)
+        btnsignup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
 
-        signupbttn.setOnClickListener {
-            val intent = Intent(this, SignupTr::class.java)
-            startActivity(intent)
+        btnlogin.setOnClickListener {
+
+            val email = etemail.text.toString().trim()
+            val password = etpassword.text.toString().trim()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            RetrofitClient.instance.login(email, password).enqueue(object :
+                Callback<LoginResponse> {
+                override fun onResponse(
+                    call: Call<LoginResponse>,
+                    response: Response<LoginResponse>
+                ){
+
+                    if(response.isSuccessful) {
+                        Toast.makeText(
+                            applicationContext,
+                            response.body()?.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        val intent = Intent(applicationContext, UserActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(
+                            applicationContext,
+                            response.body()?.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Something went wrong.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            })
         }
-
-
     }
 }
--- app/src/main/java/com/example/kitharam/Auth/SignupTr.kt
+++ app/src/main/java/com/example/kitharam/Auth/SignupTr.kt
@@ -6,8 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.kitharam.Frontauth
 import com.example.kitharam.R
+import com.example.kitharam.api.DefaultResponse
+import com.example.kitharam.api.RetrofitClient
 import com.example.kitharam.databinding.ActivitySignupTrBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class SignupTr : AppCompatActivity() {
@@ -17,7 +24,7 @@ class SignupTr : AppCompatActivity() {
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
     private lateinit var etConfirmation: EditText
-    private lateinit var btnRegister: Button
+    private lateinit var btnSignUptr: Button
     private lateinit var btnlogin: Button
 
     private var username: String = ""
@@ -34,74 +41,86 @@ class SignupTr : AppCompatActivity() {
         etEmail = findViewById(R.id.etemail)
         etPassword = findViewById(R.id.etpassword)
         etConfirmation = findViewById(R.id.etConpassword)
-        btnRegister = findViewById(R.id.btnSignup)
         btnlogin = findViewById(R.id.btnlogin)
+        btnSignUptr = findViewById(R.id.btnSignup)
 
-        binding.btnSignup.setOnClickListener {
-            checkRegister()
-        }
         btnlogin.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
+            val intent = Intent(this, Frontauth::class.java)
             startActivity(intent)
         }
-    }
 
-    private fun checkRegister() {
-        username = etUsername.text.toString()
-        email = etEmail.text.toString()
-        password = etPassword.text.toString()
-        confirmation = etConfirmation.text.toString()
+        btnSignUptr.setOnClickListener {
 
-        if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
-            alertFail("Name, Email and Password is Required")
-        } else if (password != confirmation) {
-            alertFail("Password and Password Confirmation doesn't match.")
-        } else {
-//            callRegisterApi(username, email, password)
-            true
+            val name = etUsername.text.toString().trim()
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+            val cpassword = etConfirmation.text.toString().trim()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || cpassword.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password != cpassword) {
+                Toast.makeText(this, "Password do not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            RetrofitClient.instance.register(name, email, password, cpassword)
+                .enqueue(object : Callback<DefaultResponse> {
+                    override fun onResponse(
+                        call: Call<DefaultResponse>,
+                        response: Response<DefaultResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            Toast.makeText(
+                                applicationContext,
+                                response.body()?.message,
+                                Toast.LENGTH_LONG
+                            ).show()
+
+
+                        } else {
+                            val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+                            Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Email Already Exist",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                })
         }
     }
-
-//    private fun callRegisterApi(username: String, email: String, password: String) {
-//        val call: Call<RegisterModelResponse> = APIClient.getInstance().api.registerApi(username, email, password)
-//        call.enqueue(object : Callback<RegisterModelResponse> {
-//            override fun onResponse(call: Call<RegisterModelResponse>, response: Response<RegisterModelResponse>) {
-//                if (response.isSuccessful) {
-//                    Toast.makeText(this@Signup, "success", Toast.LENGTH_SHORT).show()
-//                    startActivity(Intent(this@Signup, Login::class.java))
-//                    Toast.makeText(this@Signup, response.body()?.message, Toast.LENGTH_SHORT).show()
-//                } else {
-//                    Toast.makeText(this@Signup, "Failed", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<RegisterModelResponse>, t: Throwable) {
-//                Toast.makeText(this@Signup, "error", Toast.LENGTH_SHORT).show()
-//            }
-//        })
-//    }
-
-    private fun alertSuccess(s: String) {
-        AlertDialog.Builder(this)
-            .setTitle("Success")
-            .setIcon(R.drawable.baseline_email_24)
-            .setMessage(s)
-            .setPositiveButton("Login") { dialog, which ->
-                dialog.dismiss()
-                val intent = Intent(this, Login::class.java)
-                startActivity(intent)
-            }
-            .show()
-    }
-
-    private fun alertFail(s: String) {
-        AlertDialog.Builder(this)
-            .setTitle("Failed")
-            .setIcon(R.drawable.baseline_info_24)
-            .setMessage(s)
-            .setPositiveButton("OK") { dialog, which ->
-                dialog.dismiss()
-            }
-            .show()
-    }
 }
+
+//    private fun alertSuccess(s: String) {
+//        AlertDialog.Builder(this)
+//            .setTitle("Success")
+//            .setIcon(R.drawable.baseline_email_24)
+//            .setMessage(s)
+//            .setPositiveButton("Login") { dialog, which ->
+//                dialog.dismiss()
+//                val intent = Intent(this, Login::class.java)
+//                startActivity(intent)
+//            }
+//            .show()
+//    }
+//
+//    private fun alertFail(s: String) {
+//        AlertDialog.Builder(this)
+//            .setTitle("Failed")
+//            .setIcon(R.drawable.baseline_info_24)
+//            .setMessage(s)
+//            .setPositiveButton("OK") { dialog, which ->
+//                dialog.dismiss()
+//            }
+//            .show()
+//    }
+//}
